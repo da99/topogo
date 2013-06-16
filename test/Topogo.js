edc00b19f266daaa388488df17db5cee630c45f5
da99 <i-hate-spam-1234567@mailinator.com>
2013-06-16 01:25:19 -0500

    Refactored: .on_dup is a function. Added tests.

--- test/Topogo.js
+++ test/Topogo.js
@@ -34,313 +34,326 @@ function is_recent(date) {
 
 function rand() { return parseInt(Math.random() * 1000); }
 
-describe( 'Topogo', function () {
 
-  before(function (done) {
-    R(done)
+before(function (done) {
+  R(done)
+  .job(function (j) {
+    Topogo.run("DROP TABLE IF EXISTS \"" + table + "\";", [], j);
+  })
+  .job(function (j) {
+    Topogo.run("CREATE TABLE IF NOT EXISTS \"" + table +
+               "\" (\n" +
+               " id serial PRIMARY KEY, \n" +
+               " name varchar(10), \n" +
+               " body text ,   \n" +
+               " $created_at , \n" +
+               " $updated_at , \n" +
+               " $trashed_at   \n);", [], j);
+  })
+  .run();
+});
+
+var name = "ro ro" + rand();
+var body = "body: " + rand();
+var id   = "wrong_id";
+
+
+
+before(function (done) {
+  R(done)
+  .job(function (j) {
+    var sql = 'INSERT INTO \"' + table +  '\" (name, body) VALUES ($1, $2) RETURNING * ;';
+    Topogo.run(sql, [name, body], j);
+  })
+  .job(function (j, last) {
+    id = last[0].id;
+    j.finish();
+  })
+  .run();
+});
+
+after(function (done) {
+  R(done)
+  .job(function (j) {
+    Topogo.run("DELETE FROM \"" + table + "\";", [], j);
+  })
+  .run();
+});
+
+describe( '.run', function () {
+  it( 'uses process.DATABASE_URL by default', function (done) {
+    River.new(null)
     .job(function (j) {
-      Topogo.run("DROP TABLE IF EXISTS \"" + table + "\";", [], j);
+      Topogo.run("SELECT now()", {}, j);
     })
+    .job(function (j, result) {
+      assert.equal(H.is_date(result[0].now), true);
+      done();
+    })
+    .run();
+  });
+}); // === end desc
+
+// ================================================================
+// ================== CREATE ======================================
+// ================================================================
+
+
+describe( '.create', function () {
+  it( 'inserts object as row', function (done) {
+    var body = Math.random(1000) + "";
+    River.new(null)
     .job(function (j) {
-      Topogo.run("CREATE TABLE IF NOT EXISTS \"" + table +
-                 "\" (\n" +
-                 " id serial PRIMARY KEY, \n" +
-                 " name varchar(10), \n" +
-                 " body text ,   \n" +
-                 " $created_at , \n" +
-                 " $updated_at , \n" +
-                 " $trashed_at   \n);", [], j);
+      Topogo.new(table).create({name: "hi 1", body: body}, j);
+    })
+    .job(function (j, result) {
+      assert.equal(result.id > 0, true);
+      assert.equal(result.body, body);
+      done();
     })
     .run();
   });
 
-  var name = "ro ro" + rand();
-  var body = "body: " + rand();
-  var id   = "wrong_id";
-
-
-
-  before(function (done) {
-    R(done)
-    .job(function (j) {
-      var sql = 'INSERT INTO \"' + table +  '\" (name, body) VALUES ($1, $2) RETURNING * ;';
-      Topogo.run(sql, [name, body], j);
-    })
-    .job(function (j, last) {
-      id = last[0].id;
-      j.finish();
-    })
-    .run();
-  });
-
-  after(function (done) {
-    R(done)
-    .job(function (j) {
-      Topogo.run("DELETE FROM \"" + table + "\";", [], j);
-    })
-    .run();
-  });
-
-  describe( '.run', function () {
-    it( 'uses process.DATABASE_URL by default', function (done) {
-      River.new(null)
-      .job(function (j) {
-        Topogo.run("SELECT now()", {}, j);
-      })
-      .job(function (j, result) {
-        assert.equal(H.is_date(result[0].now), true);
-        done();
-      })
-      .run();
+  it( 'runs .on_dup function if duplicate field name is created', function (done) {
+    var body = Math.random(1000) + "";
+    var t    = Topogo.new(table);
+    t.on_dup('id', function (name) {
+      assert.equal("id", name);
+      done();
     });
-  }); // === end desc
 
-  // ================================================================
-  // ================== CREATE ======================================
-  // ================================================================
+    River.new(null)
+    .job(function (j) {
+      t.create({id: 1, name: "hi 1", body: body}, j);
+    })
+    .run();
+  });
+}); // === end desc
 
 
-  describe( '.create', function () {
-    it( 'inserts object as row', function (done) {
-      var body = Math.random(1000) + "";
+// ================================================================
+// ================== READ ========================================
+// ================================================================
+
+describe( '.read', function () {
+
+  describe( '.read_by_id', function () {
+
+    it( 'returns a single result', function (done) {
       River.new(null)
       .job(function (j) {
-        Topogo.new(table).create({name: "hi 1", body: body}, j);
+        T.read_by_id(id, j);
       })
       .job(function (j, result) {
-        assert.equal(result.id > 0, true);
+        assert.equal(result.id, id);
         assert.equal(result.body, body);
         done();
       })
       .run();
     });
+
   }); // === end desc
 
+  describe( '.read_one', function () {
 
-  // ================================================================
-  // ================== READ ========================================
-  // ================================================================
+    it( 'returns a single result', function (done) {
+      River.new(null)
+      .job(function (j) {
+        T.read_one({body: body}, j);
+      })
+      .job(function (j, last) {
+        assert.equal(last.id, id);
+        done();
+      })
+      .run();
+    });
+  }); // === end desc
 
-  describe( '.read', function () {
+  describe( '.read_list', function () {
 
-    describe( '.read_by_id', function () {
+    it( 'returns a list', function (done) {
+      River.new(null)
+      .job(function (j) {
+        T.read_list({body: body}, j);
+      })
+      .job(function (j, last) {
+        assert.equal(last.length, 1);
+        done();
+      })
+      .run();
+    });
 
-      it( 'returns a single result', function (done) {
-        River.new(null)
-        .job(function (j) {
-          T.read_by_id(id, j);
-        })
-        .job(function (j, result) {
-          assert.equal(result.id, id);
-          assert.equal(result.body, body);
-          done();
-        })
-        .run();
+  }); // === end desc
+
+}); // === end desc
+
+
+
+// ================================================================
+// ================== UPDATE ======================================
+// ================================================================
+
+
+describe( '.update', function () {
+
+  it( 'updates record with string id', function (done) {
+    body = "new body " + rand();
+    River.new(null)
+    .job(function (j) {
+      T.update(id.toString(), {body: body}, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.id, id);
+      Q.query('SELECT * from \"' + table + '\" WHERE body = $1 LIMIT 1;', [body], function (err, result) {
+        if (err) throw err;
+        var row = result.rows[0];
+        assert.equal(row.body, body);
+        assert.equal(row.id, id);
+        done();
       });
+    }).run();
+  });
 
-    }); // === end desc
+}); // === end desc
 
-    describe( '.read_one', function () {
-
-      it( 'returns a single result', function (done) {
-        River.new(null)
-        .job(function (j) {
-          T.read_one({body: body}, j);
-        })
-        .job(function (j, last) {
-          assert.equal(last.id, id);
-          done();
-        })
-        .run();
+describe( '.update_and_stamp', function () {
+  it( 'updates record with time stamp', function (done) {
+    body = "new body " + rand();
+    River.new(null)
+    .job(function (j) {
+      T.update_and_stamp(id.toString(), {body: body}, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.id, id);
+      Q.query('SELECT * from \"' + table + '\" WHERE body = $1 LIMIT 1;', [body], function (err, result) {
+        if (err) throw err;
+        var row = result.rows[0];
+        assert.equal(row.body, body);
+        assert.equal(row.id, id);
+        assert.equal(is_recent(row.updated_at), true);
+        done();
       });
-    }); // === end desc
+    }).run();
+  });
+}); // === end desc
 
-    describe( '.read_list', function () {
+// ================================================================
+// ================== Trash/Untrash ===============================
+// ================================================================
 
-      it( 'returns a list', function (done) {
-        River.new(null)
-        .job(function (j) {
-          T.read_list({body: body}, j);
-        })
-        .job(function (j, last) {
-          assert.equal(last.length, 1);
-          done();
-        })
-        .run();
-      });
+describe( '.trash', function () {
+  it( 'updates column trashed_at to: timestamp epoch', function (done) {
 
-    }); // === end desc
+    var l = ((new Date).getTime() + '').length;
 
-  }); // === end desc
+    River.new(null)
+    .job(function (j) {
+      T.trash(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(is_recent(last.trashed_at), true);
+      j.finish();
+    })
+    .job(function (j) {
+      T.read_by_id(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(is_recent(last.trashed_at), true);
+      done();
+    })
+    .run();
+  });
 
+  it( 'passes first record to next job', function (done) {
+    River.new(null)
+    .job(function (j) {
+      T.trash(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.id, id);
+      done();
+    })
+    .run();
+  });
+}); // === end desc
 
+describe( '.untrash', function () {
+  it( 'updates column trashed_at to: null', function (done) {
+    River.new(null)
+    .job(function (j) {
+      T.trash(id, j);
+    })
+    .job(function (j) {
+      T.untrash(id, j);
+    })
+    .job(function (j, last) {
+      T.read_by_id(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.trashed_at, null);
+      assert.equal(last.id, id);
+      done();
+    })
+    .run();
+  });
 
-  // ================================================================
-  // ================== UPDATE ======================================
-  // ================================================================
+  it( 'passes first record to next job', function (done) {
+    River.new(null)
+    .job(function (j) {
+      T.untrash(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.id, id);
+      done();
+    })
+    .run();
+  });
+}); // === end desc
 
+describe( '.delete_trashed', function () {
 
-  describe( '.update', function () {
+  it( 'does not delete records younger than days specified', function (done) {
+    var day_4 = H.days_ago(4);
+    var day_almost_4 = H.days_ago(4, 3000);
 
-    it( 'updates record with string id', function (done) {
-      body = "new body " + rand();
-      River.new(null)
-      .job(function (j) {
-        T.update(id.toString(), {body: body}, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.id, id);
-        Q.query('SELECT * from \"' + table + '\" WHERE body = $1 LIMIT 1;', [body], function (err, result) {
-          if (err) throw err;
-          var row = result.rows[0];
-          assert.equal(row.body, body);
-          assert.equal(row.id, id);
-          done();
-        });
-      }).run();
-    });
+    River.new(null)
+    .job(function (j) {
+      T.update(id, {trashed_at: day_almost_4}, j);
+    })
+    .job(function (j, last) {
+      T.delete_trashed(4, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.length, 0);
+      j.finish();
+    })
+    .job(function (j) {
+      T.read_by_id(id, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last.id, id);
+      done();
+    })
+    .run();
+  });
 
-  }); // === end desc
-
-  describe( '.update_and_stamp', function () {
-    it( 'updates record with time stamp', function (done) {
-      body = "new body " + rand();
-      River.new(null)
-      .job(function (j) {
-        T.update_and_stamp(id.toString(), {body: body}, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.id, id);
-        Q.query('SELECT * from \"' + table + '\" WHERE body = $1 LIMIT 1;', [body], function (err, result) {
-          if (err) throw err;
-          var row = result.rows[0];
-          assert.equal(row.body, body);
-          assert.equal(row.id, id);
-          assert.equal(is_recent(row.updated_at), true);
-          done();
-        });
-      }).run();
-    });
-  }); // === end desc
-
-  // ================================================================
-  // ================== Trash/Untrash ===============================
-  // ================================================================
-
-  describe( '.trash', function () {
-    it( 'updates column trashed_at to: timestamp epoch', function (done) {
-
-      var l = ((new Date).getTime() + '').length;
-
-      River.new(null)
-      .job(function (j) {
-        T.trash(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(is_recent(last.trashed_at), true);
-        j.finish();
-      })
-      .job(function (j) {
-        T.read_by_id(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(is_recent(last.trashed_at), true);
-        done();
-      })
-      .run();
-    });
-
-    it( 'passes first record to next job', function (done) {
-      River.new(null)
-      .job(function (j) {
-        T.trash(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.id, id);
-        done();
-      })
-      .run();
-    });
-  }); // === end desc
-
-  describe( '.untrash', function () {
-    it( 'updates column trashed_at to: null', function (done) {
-      River.new(null)
-      .job(function (j) {
-        T.trash(id, j);
-      })
-      .job(function (j) {
-        T.untrash(id, j);
-      })
-      .job(function (j, last) {
-        T.read_by_id(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.trashed_at, null);
-        assert.equal(last.id, id);
-        done();
-      })
-      .run();
-    });
-
-    it( 'passes first record to next job', function (done) {
-      River.new(null)
-      .job(function (j) {
-        T.untrash(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.id, id);
-        done();
-      })
-      .run();
-    });
-  }); // === end desc
-
-  describe( '.delete_trashed', function () {
-
-    it( 'does not delete records younger than days specified', function (done) {
-      var day_4 = H.days_ago(4);
-      var day_almost_4 = H.days_ago(4, 3000);
-
-      River.new(null)
-      .job(function (j) {
-        T.update(id, {trashed_at: day_almost_4}, j);
-      })
-      .job(function (j, last) {
-        T.delete_trashed(4, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.length, 0);
-        j.finish();
-      })
-      .job(function (j) {
-        T.read_by_id(id, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last.id, id);
-        done();
-      })
-      .run();
-    });
-
-    it( 'deletes records older than days specified', function (done) {
-      var day_3 = H.days_ago(3);
-      River.new(null)
-      .job(function (j) {
-        T.update(id, {trashed_at: day_3}, j);
-      })
-      .job(function (j, last) {
-        T.delete_trashed(3, j);
-      })
-      .job(function (j, last) {
-        assert.equal(last[0].id, id);
-        done();
-      })
-      .run();
-    });
+  it( 'deletes records older than days specified', function (done) {
+    var day_3 = H.days_ago(3);
+    River.new(null)
+    .job(function (j) {
+      T.update(id, {trashed_at: day_3}, j);
+    })
+    .job(function (j, last) {
+      T.delete_trashed(3, j);
+    })
+    .job(function (j, last) {
+      assert.equal(last[0].id, id);
+      done();
+    })
+    .run();
+  });
 
 
-  }); // === end desc
 }); // === end desc
 
 
